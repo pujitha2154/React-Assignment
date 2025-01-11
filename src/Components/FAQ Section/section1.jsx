c5f54aac6f1eff5efe18f36930fa3c5f8c0375b1
Pujitha Molabanti <154341936+pujitha2154@users.noreply.github.com>
2025-01-11 17:40:35 +0530

    Add files via upload

---
+++ src/Components/FAQ Section/section1.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './section1.css';
+const Section1 = () => {
+  return (
+    <>
+    <div className='text'>
+        <h3>Frequently asked questions</h3>
+        <p>Everything you need to know about the product and Billing.</p>
+    </div>
+    </>
+  )
+}
+
+export default Section1
